90815107ded7d6967273e70a6fccd3b0c04f394b
Mateo <anoond@tutanota.com>
2024-03-12 23:15:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
-git clone https://github.com/vim/vim.git
-apt install libncurses-dev
-cd vim/
-./configure --enable-multibyte --enable-gui=gtk2 --prefix=/usr/local
-make
-make install
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cd ../
 cp .vimrc ~/
-rm -rf vim/
